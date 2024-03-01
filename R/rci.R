40828f719e9f394ac7d24dedc38c2552af8e8f9a
mathijsdeen <mathijsdeen@gmail.com>
2024-03-01 16:43:15 +0100

    add rci function

---
+++ R/rci.R
@@ -0,0 +1,19 @@
+#' @title Reliable change index
+#' @description computes the reliable change index according to Jacobson and Truax (1992).
+#' @param x1 prescore
+#' @param x2 postscore, same length as \code{x1}
+#' @param rxx internal consistency statistic
+#' @return \code{rci} returns a vector of \code{length(x1)} with reliable change index scores
+#' @references
+#' \itemize{
+#' \item{Jacobson, N.S., & Truax, P. (1992). Clinical significance: a statistical approach to defining meaningful change in psychotherapy research. *Journal of Consulting and Clinical Psychology, 59*, 12-19.}}
+#' @examples \dontrun{
+#' library(MASS)
+#' set.seed(1)
+#' q <- mvrnorm(n=120, mu=c(40, 50), Sigma = matrix(c(56.25,45,45,56.25), ncol = 2), empirical = TRUE)
+#' cbind(q, rci(q[,1], q[,2], .8), rci(q[,1], q[,2], .8) > 1.96)}
+#' @author Mathijs Deen
+#' @export
+rci <- function(x1, x2, rxx){
+  return((x2 - x1) / sqrt(2 * sd(x1, na.rm = TRUE)^2 * (1 - rxx)))
+}
