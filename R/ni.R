d6585696917d759fbfcf696146f1840aa8eaa5af
Mathijs Deen <mathijsdeen@gmail.com>
2024-03-14 16:45:55 +0100

    change name of %nin% to %ni%

--- R/ni.R
+++ R/ni.R
@@ -1,14 +1,14 @@
-#' @title %nin% function
+#' @title %ni% function
 #' @description Evaluates whether the left hand side argument is not in the right hand side argument.
 #' @param lhs left hand side
 #' @param rhs right hand side
 #' @return The function returns a vector of the same length as \code{lhs}.
-#' @details The \code{\%nin\%} function negates the \code{\%in\%} operator.
+#' @details The \code{\%ni\%} function negates the \code{\%in\%} operator.
 #' @examples
-#' c(1,2,3) %nin% c(1,2)
+#' c(1,2,3) %ni% c(1,2)
 #' @author Mathijs Deen
 #' @export
-`%nin%` <- function(lhs, rhs){
+`%ni%` <- function(lhs, rhs){
   `%notin%` <- Negate(`%in%`)
   return(lhs %notin% rhs)
 }
