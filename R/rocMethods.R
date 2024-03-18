4e04da5fdc73f7ebca5fc4d666cb1e44a8bf078c
Mathijs Deen <mathijsdeen@gmail.com>
2024-03-18 15:10:14 +0100

    remove dependency on pROC package

---
+++ R/rocMethods.R
@@ -0,0 +1,14 @@
+#' @title Print roc object
+#'
+#' @param x roc object
+#' @param ... other arguments
+#'
+#' @return prints an roc object
+#' @export
+#'
+#' @examples\dontrun{
+#' 1}
+#' @author Mathijs Deen
+print.rocc <- function(x, ...) {
+  print(x)
+}
