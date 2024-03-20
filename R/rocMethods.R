7e2cc77787e78ac6df5c4ac65f0a0447865fe749
Mathijs Deen <mathijsdeen@gmail.com>
2024-03-20 14:50:13 +0100

    add which, orientation args and plot options

--- R/rocMethods.R
+++ R/rocMethods.R
@@ -2,6 +2,9 @@
 #' @param x object of class \code{roc}.
 #' @param y argument for generic \code{plot} function, not used here.
 #' @param ... other arguments for generic \code{plot} function, none are used here.
+#' @param which which plots to show (see Details).
+#' @param orientation indicate whether the plots should be arranged horizontally
+#'     or vertically.
 #' @param cutoffs.1 cutoff value(s) to be shown in the first plot
 #' @param cutoffs.2 cutoff value(s) to be shown in the second plot
 #' @param cutoffs.3 cutoff value(s) to be shown in the third plot
@@ -26,96 +29,100 @@
 #'      cutoffs.2 = 14.5, cutoffs.3 = 14.5)
 #' @author Mathijs Deen
 #' @export
-plot.roc <- function(x, y, ..., cutoffs.1=NULL,
+plot.roc <- function(x, y, ..., which = 1:3,
+                     orientation = c("horizontal", "vertical"),
+                     cutoffs.1=NULL,
                      cutoffs.2=NULL, cutoffs.3=NULL,
                      xlab.3=NULL,
                      labels.3=NULL, xlim.3=NULL,
                      ylim.3=c(0,10), pos.legend.2="right",
                      pos.legend.3="topright") {
-  oldpty <- par()$pty
-  oldmfrow <- par()$mfrow
-  par(pty="s")
-  par(mfrow=c(1,3))
-  #hier nog een on.exit
-  #gebruiker ook nog kunnen laten kiezen welke plot(s) er moet(en) verschijnen,
-  #en hoe (verticaal, horizontaal, diagonaal)
+  or <- match.arg(arg = orientation, several.ok = FALSE)
+  nPlots <- length(which)
+  ifelse(or == "horizontal",
+         mfDim  <- c(1,nPlots),
+         mfDim  <- c(nPlots,1))
+  oldpar <- par(mfrow = mfDim,
+                pty   = "s")
+  on.exit(par(oldpar), add = TRUE)
 
   rr2 <- x$rdf
   rr2$falsePosRates <- 1 - x$rdf$specificities
 
-  #plot 1
-  plot(x    = rr2$falsePosRates,
-       y    = rr2$sensitivities,
-       ylim = c(0,1),
-       xlim = c(0,1),
-       xlab = "False positive rate (1 - specificity)",
-       ylab = "True positive rate (sensitivity)",
-       type = "l")
-  abline(a=0,b=1,col="grey")
+  if(1 %in% which){
+    plot(x    = rr2$falsePosRates,
+         y    = rr2$sensitivities,
+         ylim = c(0,1),
+         xlim = c(0,1),
+         xlab = "False positive rate (1 - specificity)",
+         ylab = "True positive rate (sensitivity)",
+         type = "l")
+    abline(a=0,b=1,col="grey")
 
-  if(!is.null(cutoffs.1)){
-    trueCO.1 <- cutoffs.1 * NA
-    for(i in 1:length(cutoffs.1)){
-      trueCO.1[i] <- rr2$thresholds[which.min(abs(cutoffs.1[i]-rr2$thresholds))[1]]
+    if(!is.null(cutoffs.1)){
+      trueCO.1 <- cutoffs.1 * NA
+      for(i in 1:length(cutoffs.1)){
+        trueCO.1[i] <- rr2$thresholds[which.min(abs(cutoffs.1[i]-rr2$thresholds))[1]]
+      }
+      points(x    = rr2$falsePosRates[which(rr2$thresholds %in% trueCO.1)],
+             y    = rr2$sensitivities[which(rr2$thresholds %in% trueCO.1)])
+      text(x      = rr2$falsePosRates[which(rr2$thresholds %in% trueCO.1)],
+           y      = rr2$sensitivities[which(rr2$thresholds %in% trueCO.1)],
+           labels = cutoffs.1,
+           adj    = c(-0.5,1))
     }
-    points(x    = rr2$falsePosRates[which(rr2$thresholds %in% trueCO.1)],
-           y    = rr2$sensitivities[which(rr2$thresholds %in% trueCO.1)])
-    text(x      = rr2$falsePosRates[which(rr2$thresholds %in% trueCO.1)],
-         y      = rr2$sensitivities[which(rr2$thresholds %in% trueCO.1)],
-         labels = cutoffs.1,
-         adj    = c(-0.5,1))
+    text(x      = .6,
+         y      = .3,
+         labels = sprintf("AUC = %.3f", x$auc))
   }
-  text(x      = .6,
-       y      = .3,
-       labels = sprintf("AUC = %.3f", x$auc))
 
-  #plot 2
-  plot(x    = rr2$thresholds,
-       y    = rr2$sensitivities,
-       type = "l",
-       ylim = c(0,1),
-       xlim = c(min(rr2$thresholds[is.finite(rr2$thresholds)]),
-                max(rr2$thresholds[is.finite(rr2$thresholds)])),
-       xlab = "Thresholds",
-       ylab = "Sensitivity, Specificity",
-       lty  = 1)
-  lines(x   = rr2$thresholds,
-        y   = rr2$specificities,
-        lty = 2)
-  legend(x      = pos.legend.2,
-         legend = c("Sensitivity","Specificity"),
-         lty    = c(1,2))
-  abline(v   = cutoffs.2,
-         lty = 3)
-  abline(h   = -.01,
-         col = "grey")
-  abline(h   = 1.01,
-         col = "grey")
+  if(2 %in% which){
+    plot(x    = rr2$thresholds,
+         y    = rr2$sensitivities,
+         type = "l",
+         ylim = c(0,1),
+         xlim = c(min(rr2$thresholds[is.finite(rr2$thresholds)]),
+                  max(rr2$thresholds[is.finite(rr2$thresholds)])),
+         xlab = "Thresholds",
+         ylab = "Sensitivity, Specificity",
+         lty  = 1)
+    lines(x   = rr2$thresholds,
+          y   = rr2$specificities,
+          lty = 2)
+    legend(x      = pos.legend.2,
+           legend = c("Sensitivity","Specificity"),
+           lty    = c(1,2))
+    abline(v   = cutoffs.2,
+           lty = 3)
+    abline(h   = -.01,
+           col = "grey")
+    abline(h   = 1.01,
+           col = "grey")
+  }
 
-  #plot 3
-  ifelse(is.null(labels.3),
-         legendnames <- levels(x$group),
-         legendnames <- labels.3)
-  ifelse(is.null(xlim.3),
-         xlm3 <- c(min(x$response),max(x$response)),
-         xlm3 <- xlim.3)
-  plot(x    = density(x$response[x$group==levels(x$group)[1]]),
-       xlim = xlm3,
-       main = "",
-       xlab = xlab.3,
-       lty  = 1,
-       ylim = ylim.3)
-  lines(x   = density(x$response[x$group==levels(x$group)[2]]),
-        lty = 2)
-  legend(x      = pos.legend.3,
-         legend = legendnames,
-         lty    = c(1,2))
-  abline(v   = cutoffs.3,
-         lty = 3)
-  abline(h   = 0,
-         col ="grey")
-  par(pty = oldpty)
-  par(mfrow = oldmfrow)
+  if(3 %in% which){
+    ifelse(is.null(labels.3),
+           legendnames <- levels(x$group),
+           legendnames <- labels.3)
+    ifelse(is.null(xlim.3),
+           xlm3 <- c(min(x$response),max(x$response)),
+           xlm3 <- xlim.3)
+    plot(x    = density(x$response[x$group==levels(x$group)[1]]),
+         xlim = xlm3,
+         main = "",
+         xlab = ifelse(is.null(xlab.3), "", xlab.3),
+         lty  = 1,
+         ylim = ylim.3)
+    lines(x   = density(x$response[x$group==levels(x$group)[2]]),
+          lty = 2)
+    legend(x      = pos.legend.3,
+           legend = legendnames,
+           lty    = c(1,2))
+    abline(v   = cutoffs.3,
+           lty = 3)
+    abline(h   = 0,
+           col ="grey")
+  }
 }
 
 #' @title Area under the curve
