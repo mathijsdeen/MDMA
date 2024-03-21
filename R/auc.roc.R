4872d7034a7d39bc8810064724abee445cdfd630
Mathijs Deen <mathijsdeen@gmail.com>
2024-03-21 16:16:36 +0100

    use separate source files for all functions

---
+++ R/auc.roc.R
@@ -0,0 +1,21 @@
+#' @title Area under the curve
+#' @description Calculate the area under the curve
+#'
+#' `r lifecycle::badge("stable")`
+#' @param x object of class \code{roc}.
+#' @param ... other arguments (none are used at the moment).
+#'
+#' @return returns the area under the curve for a \code{roc} class object.
+#' @examples
+#' a <- roc(QIDS$QIDS, QIDS$depression, c("Yes","No"), "Yes")
+#' auc(a)
+#' @author Mathijs Deen
+#' @export
+auc <- function(x, ...){
+  UseMethod("auc")
+}
+
+#' @export
+auc.roc <- function(x, ...){
+  return(x$auc)
+}
--- R/summary.tTest.R
+++ R/summary.tTest.R
@@ -43,21 +43,3 @@ summary.tTest <- function(object, ..., rnd = 3L){
   outf <- paste0("t(%.", dfRnd, "f) = %.", tRnd, "f, %s")
   cat(sprintf(outf, x$parameter, x$statistic, pFinal))
 }
-
-#' @title Print t test
-#' @description Print the output of a t test
-#'
-#' `r lifecycle::badge("stable")`
-#' @param x an object used to select a method
-#' @param ... further arguments passed to or from other methods
-#' @return prints the \code{tTest} object as a \code{htest} object.
-#' @export
-#' @examples
-#' x1 <- QIDS$QIDS[QIDS$depression == "Yes"]
-#' x2 <- QIDS$QIDS[QIDS$depression == "No"]
-#' tt <- tTest(x1, x2)
-#' print(tt)
-#' @author Mathijs Deen
-print.tTest <-function(x, ...){
-  NextMethod()
-}
