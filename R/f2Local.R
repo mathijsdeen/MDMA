2162a526e0957865ec0424490690c6108f377a14
Mathijs Deen <mathijsdeen@gmail.com>
2025-03-03 17:02:43 +0100

    add local f^2 for glm and mlogit

--- R/f2Local.R
+++ R/f2Local.R
@@ -5,33 +5,58 @@ f2Local <- function(object, ...){
 f2Local.lm <- function(object, method = r.squared){
   method <- deparse(substitute(method))
   valid_methods <- c("r.squared", "adj.r.squared")
-  if(!method %in% valid_methods) stop("invalid method", call. = FALSE)
+  if(!method %in% valid_methods) stop("invalid method.", call. = FALSE)
 
   R2full <- summary(object)[[method]]
   preds <- attr(terms(object), which = "term.labels")
   R2preds <- rep(NA_real_, length(preds))
 
   for(pred in seq_along(preds)){
-    R2preds[pred] <- m.full |>
+    R2preds[pred] <- object |>
       update(as.formula(paste(". ~ . -", preds[pred]))) |>
       summary() |>
-      (\(s) s$r.squared)()
+      (\(s) s[[method]])()
   }
 
   f2s <- (R2full - R2preds) / (1 - R2full)
-  out <- tibble(variable = preds,
-                    f2Local  = f2s)
+  out <- list(variable = preds,
+              f2Local  = f2s)
   class(out) <- "f2Local"
   return(out)
 }
 
+f2Local.glm <- function(object, method = "r2") {
+  if (is.character(method)) {
+    method_fun <- get(method, envir = asNamespace("performance"), inherits = FALSE)
+  } else if (is.function(method)) {
+    method_fun <- method
+  } else {
+    stop("argument 'method' be either a function or a string.", call. = FALSE)
+  }
+
+  R2full <- as.numeric(method_fun(object))
+  preds <- attr(terms(object), which = "term.labels")
+  R2preds <- rep(NA_real_, length(preds))
+
+  for(pred in seq_along(preds)){
+    R2preds[pred] <- object |>
+      update(as.formula(paste(". ~ . -", preds[pred]))) |>
+      method_fun() |>
+      as.numeric()
+  }
+
+  f2s <- (R2full - R2preds) / (1 - R2full)
+  out <- list(variable = preds,
+              f2Local  = f2s)
+  class(out) <- "f2Local"
+  return(out)
+}
+
+f2Local.mlogit <- f2Local.glm
+
 print.f2Local <- function(x, ...){
-  f2s <- x$f2Local
-  out <- tibble(variable = x$variable, f2s)
+  out <- data.frame(variable = x$variable,
+                f2       = x$f2Local)
   print(out)
 }
 
-library(tibble)
-f2Local.lm(m.full)
-
-View(f2Local.lm(m.full))
