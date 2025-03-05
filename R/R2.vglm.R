e484ed4e504c3cff86422a3ba8da757f8987b019
Mathijs Deen <mathijsdeen@gmail.com>
2025-03-05 16:48:33 +0100

    add f^2 and R^2 functionality for ((v)g)lm objects

--- R/R2.vglm.R
+++ R/R2.vglm.R
@@ -1,3 +1,19 @@
+#' @title Calculate (pseudo) \eqn{R^2} for `vglm` objects
+#'
+#' @param model a `vglm` object.
+#' @param method method for calculation of \eqn{R^2}.
+#'
+#' @return \code{R2.vglm} returns \eqn{R^2}.
+#' @export
+#'
+#' @examples
+#' if(require("VGAM")){
+#'   fit <- vglm(Species ~ Sepal.Length,
+#'               family = multinomial(),
+#'               data = iris)
+#'   R2.vglm(fit)
+#' }
+#' @author Mathijs Deen
 R2.vglm <- function(model, method = c("mcfadden", "nagelkerke", "efron", "coxsnell", "tjur")) {
   method <- match.arg(method)
   loglik_full <- logLik(model)
@@ -5,53 +21,7 @@ R2.vglm <- function(model, method = c("mcfadden", "nagelkerke", "efron", "coxsne
 
   null_model <- tryCatch(update(model, . ~ 1), error = function(e) NULL)
   if (is.null(null_model)) {
-    warning("No fit for null model. Check model specification.")
-    return(NA)
-  }
-  loglik_null <- logLik(null_model)
-
-  r2 <- switch(
-    method,
-    "mcfadden"   = 1 - (loglik_full / loglik_null),
-    "nagelkerke" = (1 - exp((2/n) * (loglik_null - loglik_full))) / (1 - exp((2/n) * loglik_null)),
-    "coxsnell"   = 1 - exp((loglik_null - loglik_full) / n),
-    "efron"      = {
-      pred_values <- predict(model, type = "response")
-      if (is.matrix(model@y)) {
-        y_numeric <- max.col(model@y, ties.method = "first")
-      } else {
-        y_numeric <- as.numeric(model@y)
-      }
-      if (is.matrix(pred_values)) {
-        observed_probs <- pred_values[cbind(1:n, y_numeric)]
-        1 - (sum((1 - observed_probs)^2) / sum((1 - mean(observed_probs))^2))
-      } else {
-        1 - (sum((y_numeric - pred_values)^2) / sum((y_numeric - mean(y_numeric))^2))
-      }
-    },
-    "tjur"       = {
-      if (length(unique(model@y)) != 2) {
-        warning("Tjur's R^2 is only valid for binomial models.")
-        return(NA)
-      }
-      pred_values <- predict(model, type = "response")
-      mean_pred_1 <- mean(pred_values[model@y == max(model@y)])
-      mean_pred_0 <- mean(pred_values[model@y == min(model@y)])
-      mean_pred_1 - mean_pred_0
-    }
-  )
-
-  return(r2)
-}
-
-R2.vglm <- function(model, method = c("mcfadden", "nagelkerke", "efron", "coxsnell", "tjur")) {
-  method <- match.arg(method)
-  loglik_full <- logLik(model)
-  n <- nobs(model)
-
-  null_model <- tryCatch(update(model, . ~ 1), error = function(e) NULL)
-  if (is.null(null_model)) {
-    warning("No fit for null model. Check model specification.")
+    warning("No fit for null model. Check model specification.", call. = FALSE)
     return(NA)
   }
   loglik_null <- logLik(null_model)
@@ -61,35 +31,15 @@ R2.vglm <- function(model, method = c("mcfadden", "nagelkerke", "efron", "coxsne
                "nagelkerke" = (1 - exp((2/n) * (loglik_null - loglik_full))) / (1 - exp((2/n) * loglik_null)),
                "coxsnell" = 1 - exp((loglik_null - loglik_full) / n),
                "efron" = {
+                 # Controleer of het model multinomiaal is
+                 if (inherits(model@family, "multinomial")) {
+                   stop("Efron's R^2 is not suitable for multinomial models. Use McFadden, Nagelkerke, or Cox & Snell instead.")
+                 }
+
                  pred_values <- predict(model, type = "response")
+                 y_numeric <- as.numeric(model@y)
 
-                 if (is.matrix(model@y)) {
-                   y_numeric <- max.col(model@y, ties.method = "first")
-                 } else {
-                   y_numeric <- as.numeric(model@y)
-                 }
-
-                 if (is.matrix(pred_values)) {
-                   # Multinomiaal: waarschijnlijkheid van de geobserveerde klasse pakken
-                   observed_probs <- pred_values[cbind(1:n, y_numeric)]
-
-                   # Correctie: Vermijd 0'en en 1'en die de log-likelihood kunnen verpesten
-                   observed_probs <- pmax(pmin(observed_probs, 1 - 1e-10), 1e-10)
-
-                   # Gemiddelde waarschijnlijkheid van het model
-                   mean_prob <- mean(observed_probs)
-
-                   # Verbeterde schaal: gebruik log-odds als betrouwbaarder alternatief
-                   efron_score <- mean(log(observed_probs / (1 - observed_probs))) - mean(log(mean_prob / (1 - mean_prob)))
-
-                   # Terugschalen naar 0-1 range
-                   r2_value <- 1 / (1 + exp(-efron_score))
-
-                   r2_value
-                 } else {
-                   # Standaard Efron voor binomiale/continue modellen
-                   1 - (sum((y_numeric - pred_values)^2) / sum((y_numeric - mean(y_numeric))^2))
-                 }
+                 1 - (sum((y_numeric - pred_values)^2) / sum((y_numeric - mean(y_numeric))^2))
                },
                "tjur" = {
                  if (length(unique(model@y)) != 2) {
@@ -100,8 +50,6 @@ R2.vglm <- function(model, method = c("mcfadden", "nagelkerke", "efron", "coxsne
                  mean(pred_values[model@y == max(model@y)]) - mean(pred_values[model@y == min(model@y)])
                }
   )
-
+  names(r2) <- method
   return(r2)
 }
-
-
--- R/f2Local.R
+++ R/f2Local.R
@@ -1,8 +1,32 @@
-f2Local <- function(object, ...){
+#' @title Calculate local \eqn{f^2} for (generalized) linear models
+#'
+#' @param object a model object (currently supported: `lm`, `glm`, `vglm`).
+#' @param method method for calculation of \eqn{R^2}, which is needed for the calculation of \eqn{f^2}. See Details.
+#' @param ... currently not used
+#'
+#' @return \code{f2Local} returns a list containing \eqn{f^2} values for every parameter in a model.
+#' @details
+#' The following methods can be specified:
+#' * \code{lm} objects: `r.squared` and `adj.r.squared` as extracted from the `lm` object.
+#' * `glm` objects: `mcfadden`, `nagelkerke`, `coxsnell`, `tjur` and `efron`, as implemented
+#' in the `performance` package.
+#' * `vglm` objects: `mcfadden`, `nagelkerke`, `coxsnell`, `tjur` and `efron`, as implemented
+#' in the `R2.vglm` function.
+#'
+#' Note that for multinomial models, using `method="efron"` gives questionable with `glm` objects and
+#' is not possible for `vglm` objects. For `glm` objects, `method=coxsnell` cannot be used when the
+#' response is not binary.
+#' @importFrom stats as.formula logLik nobs terms update
+#' @examples
+#' model <- lm(mpg ~ cyl + wt*drat, data = mtcars)
+#' @author Mathijs Deen
+#' @export
+f2Local <- function(object, method, ...){
   UseMethod("f2Local")
 }
 
-f2Local.lm <- function(object, method = "r.squared"){
+#' @export
+f2Local.lm <- function(object, method = "r.squared", ...){
   valid_methods <- c("r.squared", "adj.r.squared")
   if(!method %in% valid_methods) stop("invalid method.", call. = FALSE)
 
@@ -24,17 +48,12 @@ f2Local.lm <- function(object, method = "r.squared"){
   return(out)
 }
 
-f2Local.glm <- function(object, method = "r2") {
-  if (is.character(method)) {
-    method_fun <- get(method, envir = asNamespace("performance"), inherits = FALSE)
-  } else if (is.function(method)) {
-    method_fun <- method
-  } else {
-    stop("argument 'method' be either a function or a string.", call. = FALSE)
-  }
+#' @export
+f2Local.glm <- function(object, method = "r2", ...) {
+  if(method != "r2") method <- paste0("r2_", method)
+  method_fun <- get(method, envir = asNamespace("performance"), inherits = FALSE)
 
   R2full <- as.numeric(method_fun(object))
-  #return(R2full)
   preds <- attr(terms(object), which = "term.labels")
   R2preds <- rep(NA_real_, length(preds))
 
@@ -42,8 +61,7 @@ f2Local.glm <- function(object, method = "r2") {
     R2preds[pred] <- object |>
       update(as.formula(paste(". ~ . -", preds[pred]))) |>
       method_fun() |>
-      as.numeric() |> round(5)
-    print(R2preds[pred])
+      as.numeric()
   }
 
   f2s <- (R2full - R2preds) / (1 - R2full)
@@ -53,29 +71,32 @@ f2Local.glm <- function(object, method = "r2") {
   return(out)
 }
 
-f2Local.vglm <- function(object, method = "mcfadden"){
-  valid_methods <- c("mcfadden", "nagelkerke", "efron")
+#' @export
+f2Local.vglm <- function(object, method = "mcfadden", ...){
+  valid_methods <- c("mcfadden", "nagelkerke", "efron", "coxsnell", "tjur")
   if(!method %in% valid_methods) stop("invalid method.", call.=FALSE)
 
-  if(method == "mcfadden"){
-    method_fun <- function(){
-      cat("mcfadden")
-    }
-  }else if(method == "nagelkerke"){
-    method_fun <- function(){
-      cat("nagelkerke")
-    }
-  }else if(method == "efron"){
-    method_fun <- function(){
-      cat("efron")
-    }
+  R2full <- R2.vglm(object, method = method)
+
+  preds <- attr(terms(object), which = "term.labels")
+  R2preds <- rep(NA_real_, length(preds))
+
+  for(pred in seq_along(preds)){
+    R2preds[pred] <- object |>
+      update(as.formula(paste(". ~ . -", preds[pred]))) |>
+      R2.vglm(method = method) |>
+      as.numeric()
   }
-  method_fun()
+  f2s <- (R2full - R2preds) / (1 - R2full)
+  out <- list(variable = preds,
+              f2Local  = f2s)
+  class(out) <- "f2Local"
+  return(out)
 }
 
+#' @export
 print.f2Local <- function(x, ...){
   out <- data.frame(variable = x$variable,
-                f2       = x$f2Local)
+                    f2       = x$f2Local)
   print(out)
 }
-
