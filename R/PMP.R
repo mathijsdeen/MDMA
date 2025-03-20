7a615a238d004f67b7f8ddf2b9e6cc64fde91396
Mathijs Deen <mathijsdeen@gmail.com>
2025-03-20 15:22:04 +0100

    rename function

--- R/PMP.R
+++ R/PMP.R
@@ -2,20 +2,26 @@
 #' @description Calculate the posterior model probability for a set of models.
 #'
 #' `r lifecycle::badge("stable")`
+#'
+#' @usage
+#' PMP(...)
+#'
+#' ##  aliases for backward compatibility, deprecated:
+#'
 #' @param ... objects of class \code{(g)lm}, given as separate arguments.
-#' @return \code{pMM} returns to posterior model probabilities for the models provided.
+#' @return \code{PMP} returns to posterior model probabilities for the models provided.
 #' @details
-#' Posterior model probabilities are calculated for every model \eqn{i} as \deqn{\mathrm{pMO}_i = \frac{\mathrm{exp}\Big[-\frac{1}{2}\Delta_i\mathrm{BIC}\Big]}{\sum_{j = 1}^K\mathrm{exp}\Big[-\frac{1}{2}\Delta_j\mathrm{BIC}\Big]},}
+#' Posterior model probabilities are calculated for every model \eqn{i} as \deqn{\mathrm{PMP}_i = \frac{\mathrm{exp}\Big[-\frac{1}{2}\Delta_i\mathrm{BIC}\Big]}{\sum_{j = 1}^K\mathrm{exp}\Big[-\frac{1}{2}\Delta_j\mathrm{BIC}\Big]},}
 #' where the minimal BIC value is subtracted from all BICs. In other words: the model with the lowest BIC has \eqn{\Delta\mathrm{BIC}=0}.
 #' @importFrom stats BIC
 #' @examples
 #' lm.1 <- lm(mpg ~ hp + wt, data = mtcars)
 #' lm.2 <- lm(mpg ~ hp * wt, data = mtcars)
 #' lm.3 <- lm(mpg ~ hp * wt + gear, data = mtcars)
-#' pMM(lm.1, lm.2, lm.3)
+#' PMP(lm.1, lm.2, lm.3)
 #' @author Mathijs Deen
 #' @export
-pMM <- function(...){
+PMP <- function(...){
   models <- list(...)
   formulae <- unlist(lapply(models, FUN = function(x) .Primitive("$")(x, call)[[2]]))
   if(any(lapply(models, function(x) sum(class(x) %in% c("lm", "glm")))==0)) {
@@ -24,8 +30,23 @@ pMM <- function(...){
   BICs <- as.matrix(lapply(models, BIC)) |> unlist()
   lowestBIC <- min(BICs)
   relBICs <- BICs - lowestBIC
-  pMM <- exp(-1/2 * relBICs)/sum(exp(-1/2 * relBICs))
-  res <- cbind(BIC = BICs, pMM)
+  pmp <- exp(-1/2 * relBICs)/sum(exp(-1/2 * relBICs))
+  res <- cbind(BIC = BICs,
+               PMP = pmp)
   rownames(res) <- formulae
   return(res)
+}
+
+#' @export
+#' @describeIn PMP deprecated alias
+pMO <- function(...){
+  .warningDeprecated("PMP", "2.0.0")
+  PMP(...)
+}
+
+#' @export
+#' @describeIn PMP deprecated alias
+pMM <- function(...){
+  .warningDeprecated("PMP", "2.0.0")
+  PMP(...)
 }
